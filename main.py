462967600d7d16ddbff15ce5cd32413cc6da728f
Polly Bel <polialekseevna80@gmail.com>
2024-10-03 09:50:36 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-x=input("Введите значение х")
-print("x=",x)
-y=input("Введите значение у")
-print("y=",y)
-z=input("Введите значение z от 0 до 1")
-print("z=",z)
-m=(int(x)-int(y))
 import math
 from math import fabs
-b=(((10*((int(x)**1/3)+(int(x)**(int(y)+2))))**1/2)*(((math.asin(float(z)))**2)-fabs(m)))
+x=int(input("Введите значение х"))
+print("x=",x)
+y=int(input("Введите значение у"))
+print("y=",y)
+z=int(input("Введите значение z от 0 до 1"))
+print("z=",z)
+m=x-y
+b=(((10*(x**1/3)+(x**(y+2)))**1/2)*(((math.asin(z))**2)-fabs(m)))
 print("Ответ:", b)
